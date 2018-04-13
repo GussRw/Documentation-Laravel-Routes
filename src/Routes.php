13bb1ef61c4fae45146aa7da4df70d93cf6a49af
GussRw <gussrw1@gmail.com>
2018-04-13 16:06:00 -0500

    first commit

---
+++ src/Routes.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guss
+ * Date: 4/13/18
+ * Time: 3:50 PM
+ */
+
+class Routes
+{
+    public static function hello()
+    {
+        return "Hola";
+    }
+}
